971676c683a2f276f483e50fef86aa278f31f606
Thom Joy <thomjoy1984@gmail.com>
2013-10-11 09:39:26 +1100

    ex2

---
+++ complete.R
@@ -0,0 +1,22 @@
+complete <- function(directory, id = 1:332) {
+  for( f in id ) {
+
+  }
+
+  infiles <- dir(file.path(getwd(), directory), pattern='\\.csv$')
+  df <- data.frame(id = numeric(), nobs = numeric())
+
+  countobs <- function(file) {
+    data <- getmonitor(file, 'specdata')
+    id <- data$ID[1]
+    nobs <- sum(complete.cases(data))
+    row <- data.frame(id = id, nobs = nobs)
+    return(row)
+  }
+
+  for( i in id ) {
+    df <- rbind(df, countobs(i))
+  }
+
+  return(df)
+}
